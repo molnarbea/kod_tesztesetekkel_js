e1ccbb585f524d07acf04a98c1df8a5071043508
molnarbea <molnarbeatrix11@gmail.com>
2026-02-25 15:06:14 +0100

    kod megirva

--- feladatok.js
+++ feladatok.js
@@ -1,5 +1,14 @@
 export function egyedi_betuk(szoveg = "") {
-  // ide kell írni a megoldást ciklussal és elágazással
- 
-  return [];
+  const lista = [];
+  for (let i = 0; i < szoveg.length; i++) {
+    let betu= szoveg[i];
+    let kisbetu=betu.toLowerCase();
+    if (/^[a-záéíóöőúüű]$/.test(kisbetu) && lista.includes(kisbetu) != true) {
+      lista.push(kisbetu);
+    }
+    
+  }
+  lista.sort();
+  console.log(lista);
+  return lista;
 }
